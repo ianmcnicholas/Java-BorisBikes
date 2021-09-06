0536d2fb3a9f51bb731afdf76fa5b20d439ee9ee
ianmcnicholas <ianjmcnicholas@.com>
2021-09-06 15:31:19 +0100

    @BeforeEach added to DockingStation tests to create a new dockingStation

--- src/test/java/DockingStationTest.java
+++ src/test/java/DockingStationTest.java
@@ -1,12 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DockingStationTest {
 
+    private DockingStation dockingStation;
+
+    @BeforeEach
+    public void setUp() {
+        dockingStation = new DockingStation();
+    }
+
     @Test
+    @DisplayName("A new docking station should be empty to start with")
     void testANewDockingStationIsEmpty() {
-        DockingStation dockingStation = new DockingStation();
         assertEquals(0, dockingStation.getBikesList().size());
+    }
+
+    @Test
+    @DisplayName("A docking station can 'dock' a bike")
+    void testCanDockABike() {
+
     }
 
 }
